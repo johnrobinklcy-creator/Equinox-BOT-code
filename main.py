ada9f7584db70a6463844d5c2903367bf5daa901
johnrobinklcy-creator <johnrobin.klcy@gmail.com>
2026-07-16 12:42:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-hi
+import discord
+from discord.ext import commands
+import json
+
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix=config["prefix"], intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is online!")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("🏓 Pong!")
+
+bot.run(config["token"])
